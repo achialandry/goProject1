6bed63d7b49f0dbb044d408b4a331ea132a5bc24
Landry <26629374+achialandry@users.noreply.github.com>
2025-12-09 19:49:27 -0700

    - created go project - created http server - parsing command line flags

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/achialandry/goProject1
+
+go 1.25.5
---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/achialandry/goProject1/internal/app"
+)
+
+func main() {
+	var port int
+	flag.IntVar(&port, "port", 8080, "go backend server port")
+	flag.Parse()
+
+	app, err := app.NewApplication()
+	if err != nil {
+		panic(err)
+	}
+
+	app.Logger.Printf("we are running our app on port %d\n", port)
+
+	http.HandleFunc("/health", HealthCheck)
+	server := &http.Server{
+		Addr:         fmt.Sprintf(":%d", port),
+		IdleTimeout:  time.Minute,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 30 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		app.Logger.Fatal(err)
+	}
+}
+
+func HealthCheck(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Status is available\n")
+}
